4becf27aeb70967157cf70c48f7b51dd69e7c26c
taogen <taogenjia@gmail.com>
2024-08-02 15:56:58 +0800

    ✨feat: Add vue-html-sfc-loader

---
+++ vuejs/vue-html-sfc-loader/Child.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h3>Child</h3>
+        <div>Hello {{who}}</div>
+        <div v-html="msg"></div>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    data: function() {
+        return {
+            who: 'world'
+        }
+    },
+    props: {
+        msg: String
+    },
+}
+</script>
+
+<style>
+</style>
